25f191ab958c04031055050abb832caf7726fcbf
Selfcode <P.Tommy2005@gmx.de>
2023-09-29 00:43:57 +0200

    Add Login Function Edit Script Team Functio Edit Script Team Name

--- js/script.js
+++ js/script.js
@@ -38,6 +38,12 @@ const config = {
                 rank: "Admin",
                 skinUrlOrPathToFile: "",
                 rankColor: "rgb(121,14,14)"
+            },
+            {
+                inGameName: "AtomOrangeEmi",
+                rank: "Admin",
+                skinUrlOrPathToFile: "",
+                rankColor: "rgb(121,14,14)",
             }
         ],
         gameDesigner: [
